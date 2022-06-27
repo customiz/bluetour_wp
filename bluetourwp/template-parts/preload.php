ea3f7e2e38e1a17d31fac75c0861f72fae5721ac
Kokiat <ccubisoft@gmail.com>
2022-06-27 16:29:43 +0700

    initial master

---
+++ bluetourwp/template-parts/preload.php
@@ -0,0 +1,44 @@
+<link rel="dns-prefetch" href="//fonts.googleapis.com">
+<link rel="dns-prefetch" href="//s.w.org">
+<?php global $triply_version; ?>
+<link rel="preload" href="<?php echo esc_url( get_theme_file_uri( 'assets/fonts/icons/triply-icon-' . $triply_version . '.woff2' ) ); ?>" as="font" crossorigin>
+<link rel="preload" href="<?php echo esc_url( get_theme_file_uri( 'assets/fonts/new/GTWalsheimPro-Bold.woff2' ) ) ?>" as="font" crossorigin>
+<link rel="preload" href="<?php echo esc_url( get_theme_file_uri( 'assets/fonts/new/TheaAmeliaRegular.woff2' ) ) ?>" as="font" crossorigin>
+<link rel="preload" href="<?php echo esc_url( get_theme_file_uri( 'assets/fonts/new/GTWalsheimPro-Regular.woff2' ) ) ?>" as="font" crossorigin>
+<link rel="preload" href="<?php echo esc_url( get_theme_file_uri( 'assets/fonts/new/GTWalsheimPro-Medium.woff2' ) ) ?>" as="font" crossorigin>
+
+<link rel="preload" href="https://fonts.gstatic.com/s/inter/v2/UcC73FwrK3iLTeHuS_fvQtMwCp50KnMa2JL7W0Q5n-wU.woff2" as="font" crossorigin>
+<link rel="preload" href="https://fonts.gstatic.com/s/inter/v2/UcC73FwrK3iLTeHuS_fvQtMwCp50KnMa0ZL7W0Q5n-wU.woff2" as="font" crossorigin>
+<link rel="preload" href="https://fonts.gstatic.com/s/inter/v2/UcC73FwrK3iLTeHuS_fvQtMwCp50KnMa2ZL7W0Q5n-wU.woff2" as="font" crossorigin>
+<link rel="preload" href="https://fonts.gstatic.com/s/inter/v2/UcC73FwrK3iLTeHuS_fvQtMwCp50KnMa1pL7W0Q5n-wU.woff2" as="font" crossorigin>
+<link rel="preload" href="https://fonts.gstatic.com/s/inter/v2/UcC73FwrK3iLTeHuS_fvQtMwCp50KnMa2pL7W0Q5n-wU.woff2" as="font" crossorigin>
+<link rel="preload" href="https://fonts.gstatic.com/s/inter/v2/UcC73FwrK3iLTeHuS_fvQtMwCp50KnMa25L7W0Q5n-wU.woff2" as="font" crossorigin>
+<link rel="preload" href="https://fonts.gstatic.com/s/inter/v2/UcC73FwrK3iLTeHuS_fvQtMwCp50KnMa1ZL7W0Q5nw.woff2" as="font" crossorigin>
+<link rel="preload" href="https://fonts.gstatic.com/s/inter/v2/UcC73FwrK3iLTeHuS_fvQtMwCp50KnMa2JL7W0Q5n-wU.woff2" as="font" crossorigin>
+<link rel="preload" href="https://fonts.gstatic.com/s/inter/v2/UcC73FwrK3iLTeHuS_fvQtMwCp50KnMa0ZL7W0Q5n-wU.woff2" as="font" crossorigin>
+<link rel="preload" href="https://fonts.gstatic.com/s/inter/v2/UcC73FwrK3iLTeHuS_fvQtMwCp50KnMa2ZL7W0Q5n-wU.woff2" as="font" crossorigin>
+<link rel="preload" href="https://fonts.gstatic.com/s/inter/v2/UcC73FwrK3iLTeHuS_fvQtMwCp50KnMa1pL7W0Q5n-wU.woff2" as="font" crossorigin>
+<link rel="preload" href="https://fonts.gstatic.com/s/inter/v2/UcC73FwrK3iLTeHuS_fvQtMwCp50KnMa2pL7W0Q5n-wU.woff2" as="font" crossorigin>
+<link rel="preload" href="https://fonts.gstatic.com/s/inter/v2/UcC73FwrK3iLTeHuS_fvQtMwCp50KnMa25L7W0Q5n-wU.woff2" as="font" crossorigin>
+<link rel="preload" href="https://fonts.gstatic.com/s/inter/v2/UcC73FwrK3iLTeHuS_fvQtMwCp50KnMa1ZL7W0Q5nw.woff2" as="font" crossorigin>
+<link rel="preload" href="https://fonts.gstatic.com/s/inter/v2/UcC73FwrK3iLTeHuS_fvQtMwCp50KnMa2JL7W0Q5n-wU.woff2" as="font" crossorigin>
+<link rel="preload" href="https://fonts.gstatic.com/s/inter/v2/UcC73FwrK3iLTeHuS_fvQtMwCp50KnMa0ZL7W0Q5n-wU.woff2" as="font" crossorigin>
+<link rel="preload" href="https://fonts.gstatic.com/s/inter/v2/UcC73FwrK3iLTeHuS_fvQtMwCp50KnMa2ZL7W0Q5n-wU.woff2" as="font" crossorigin>
+<link rel="preload" href="https://fonts.gstatic.com/s/inter/v2/UcC73FwrK3iLTeHuS_fvQtMwCp50KnMa1pL7W0Q5n-wU.woff2" as="font" crossorigin>
+<link rel="preload" href="https://fonts.gstatic.com/s/inter/v2/UcC73FwrK3iLTeHuS_fvQtMwCp50KnMa2pL7W0Q5n-wU.woff2" as="font" crossorigin>
+<link rel="preload" href="https://fonts.gstatic.com/s/inter/v2/UcC73FwrK3iLTeHuS_fvQtMwCp50KnMa25L7W0Q5n-wU.woff2" as="font" crossorigin>
+<link rel="preload" href="https://fonts.gstatic.com/s/inter/v2/UcC73FwrK3iLTeHuS_fvQtMwCp50KnMa1ZL7W0Q5nw.woff2" as="font" crossorigin>
+<link rel="preload" href="https://fonts.gstatic.com/s/inter/v2/UcC73FwrK3iLTeHuS_fvQtMwCp50KnMa2JL7W0Q5n-wU.woff2" as="font" crossorigin>
+<link rel="preload" href="https://fonts.gstatic.com/s/inter/v2/UcC73FwrK3iLTeHuS_fvQtMwCp50KnMa0ZL7W0Q5n-wU.woff2" as="font" crossorigin>
+<link rel="preload" href="https://fonts.gstatic.com/s/inter/v2/UcC73FwrK3iLTeHuS_fvQtMwCp50KnMa2ZL7W0Q5n-wU.woff2" as="font" crossorigin>
+<link rel="preload" href="https://fonts.gstatic.com/s/inter/v2/UcC73FwrK3iLTeHuS_fvQtMwCp50KnMa1pL7W0Q5n-wU.woff2" as="font" crossorigin>
+<link rel="preload" href="https://fonts.gstatic.com/s/inter/v2/UcC73FwrK3iLTeHuS_fvQtMwCp50KnMa2pL7W0Q5n-wU.woff2" as="font" crossorigin>
+<link rel="preload" href="https://fonts.gstatic.com/s/inter/v2/UcC73FwrK3iLTeHuS_fvQtMwCp50KnMa25L7W0Q5n-wU.woff2" as="font" crossorigin>
+<link rel="preload" href="https://fonts.gstatic.com/s/inter/v2/UcC73FwrK3iLTeHuS_fvQtMwCp50KnMa1ZL7W0Q5nw.woff2" as="font" crossorigin>
+<link rel="preload" href="https://fonts.gstatic.com/s/inter/v2/UcC73FwrK3iLTeHuS_fvQtMwCp50KnMa2JL7W0Q5n-wU.woff2" as="font" crossorigin>
+<link rel="preload" href="https://fonts.gstatic.com/s/inter/v2/UcC73FwrK3iLTeHuS_fvQtMwCp50KnMa0ZL7W0Q5n-wU.woff2" as="font" crossorigin>
+<link rel="preload" href="https://fonts.gstatic.com/s/inter/v2/UcC73FwrK3iLTeHuS_fvQtMwCp50KnMa2ZL7W0Q5n-wU.woff2" as="font" crossorigin>
+<link rel="preload" href="https://fonts.gstatic.com/s/inter/v2/UcC73FwrK3iLTeHuS_fvQtMwCp50KnMa1pL7W0Q5n-wU.woff2" as="font" crossorigin>
+<link rel="preload" href="https://fonts.gstatic.com/s/inter/v2/UcC73FwrK3iLTeHuS_fvQtMwCp50KnMa2pL7W0Q5n-wU.woff2" as="font" crossorigin>
+<link rel="preload" href="https://fonts.gstatic.com/s/inter/v2/UcC73FwrK3iLTeHuS_fvQtMwCp50KnMa25L7W0Q5n-wU.woff2" as="font" crossorigin>
+<link rel="preload" href="https://fonts.gstatic.com/s/inter/v2/UcC73FwrK3iLTeHuS_fvQtMwCp50KnMa1ZL7W0Q5nw.woff2" as="font" crossorigin>
